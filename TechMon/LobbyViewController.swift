0dd1702d2e3614cc6fb72b2d0ce0c1366c70907f
Souta Kawamura <soutakawamura@icloud.com>
2020-09-21 20:06:54 +0900

    first commit

---
+++ TechMon/LobbyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LobbyViewController.swift
+//  TechMon
+//
+//  Created by チップまん on 2020/09/20.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class LobbyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
