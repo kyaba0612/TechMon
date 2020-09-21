6c3a9c92c8055dfebd7e634e936ece7dd2a1057f
Souta Kawamura <soutakawamura@icloud.com>
2020-09-21 21:08:41 +0900

    first commit

--- TechMon/LobbyViewController.swift
+++ TechMon/LobbyViewController.swift
@@ -9,11 +9,68 @@
 import UIKit
 
 class LobbyViewController: UIViewController {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var staminaLabel: UILabel!
+    
+    let techMonManager = TechMonManager.shared
+    
+    var stamina: Int = 100
+    var staminaTimer: Timer!
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
+        nameLabel.text = "勇者"
+        staminaLabel.text = "\(stamina)/ 100"
+        
+        staminaTimer = Timer.scheduledTimer(
+            timeInterval: 3,
+            target: self,
+            selector: #selector(updateStaminaValue),
+            userInfo: nil,
+            repeats: true)
+        staminaTimer.fire()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        techMonManager.playBGM(fileNme: "lobby")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        techMonManager.stopBGM()
+    }
+    
+    @IBAction func toBattle(){
+        
+        if stamina >= 50{
+            
+            stamina -= 50
+            staminaLabel.text = "\(stamina) / 100"
+            performSegue(withIdentifier: "toBattle", sender: nil)
+        } else {
+            
+            let alert = UIAlertController(
+                title: "バトルに行けません",
+                message: "スタミナをためてください",
+                preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func updatesStaminaValue() {
+        
+        if stamina < 100 {
+            
+            stamina += 1
+            staminaLabel.text = "\(stamina) / 100"
+        }
     }
     
 
